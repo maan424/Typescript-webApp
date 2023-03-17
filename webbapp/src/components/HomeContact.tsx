01669b010f2baa1345d4bb2c614219278d21caa6
maan424 <max.nfard@gmail.com>
2023-03-17 05:27:37 +0100

    now !

---
+++ webbapp/src/components/HomeContact.tsx
@@ -0,0 +1,15 @@
+
+import "./HomeContact.css";
+
+const HomeContact : React.FC = () => {
+    return (
+
+        <div className="homecontact">
+       <a  href="/" ><i className="fa fa-home fa-xl " aria-hidden="true"></i></a>
+       <a className=" fa fa-angle-double-right " aria-hidden="true" href="Contact"> Contacts</a>
+        </div>
+            
+    );
+  }
+  
+  export default HomeContact;
---
+++ webbapp/src/components/Two_big_windows.tsx
@@ -0,0 +1,13 @@
+
+import "./Two_big_windows.css";
+
+const two_big_windows : React.FC = () => {
+    return (
+        <div className="two-big-windows">
+        <img src="Images/img-3.svg" alt="two people standing"/>
+        <img src="Images/img-4.svg" alt="two people standing"/>
+    </div>
+    );
+  }
+  
+  export default two_big_windows;
